69ea7cd3c29795d25420cf5620eefa2e653ba208
Shruthisdst <shruthitr.nayak@gmail.com>
2016-08-25 12:58:56 +0530

    No. 9 - insertion for db is in progress

--- generate.sh
+++ generate.sh
@@ -5,6 +5,6 @@ db="bsphabba"
 usr="root"
 pwd="mysql"
 
-echo "CREATE DATABASE IF NOT EXISTS $db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;" | /usr/bin/mysql -u$usr -p$pwd
+echo "CREATE DATABASE IF NOT EXISTS $db CHARACTER SET utf8 COLLATE utf8_general_ci;" | /usr/bin/mysql -u$usr -p$pwd
 
 php insert_book.php $host $db $usr $pwd
--- insert_book.php
+++ insert_book.php
@@ -11,9 +11,9 @@ libxml_use_internal_errors(true);
 
 $db = mysql_connect($host, $user, $password) or die("Not connected to database");
 $rs = mysql_select_db($database, $db) or die("No Database");
-mysql_query("SET NAMES utf8mb4");
+mysql_query("set names utf8");
 mysql_query("DROP TABLE IF EXISTS habba");
-mysql_query("CREATE TABLE habba (entry_id int(5) auto_increment, primary key(entry_id), book_id varchar(10))auto_increment=10001 ENGINE=MyISAM CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci");
+mysql_query("CREATE TABLE habba (book_id varchar(10), text varchar(500000)) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_general_ci");
 
 $books = scandir('books');
 foreach($books as $book)
@@ -25,7 +25,8 @@ foreach($books as $book)
 		if($file == '.' || $file == '..') continue;
 		$htmlFileName = 'books/' . $book . '/' . $file;
 		$name = (explode(".", $file));
-		$bookid = $book . '_' . $name[0];
+		$bookid = $book;
+		//~ $bookid = $book . '_' . $name[0];
 		
 		if ($htmlFileName === false)
 		{
@@ -37,14 +38,10 @@ foreach($books as $book)
 		}
 		else 
 		{
-			libxml_use_internal_errors(true);
 			$doc = new DOMDocument();
 			$doc->loadHTMLFile($htmlFileName);
-			libxml_use_internal_errors(false);
-			$finder = new DomXPath($doc);
-			$text = $doc->saveHTML();
-			echo $bookid . "\n";
-			$query = "INSERT INTO habba VALUES('', '$bookid')";
+			$text = addslashes($doc->saveHTML());
+			$query = "INSERT INTO habba VALUES('$bookid', '$text')";
 			mysql_query($query) or die("Query Problem" . mysql_error() . "\n");
 		}
 	}
